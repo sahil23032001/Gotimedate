6fed62fa1416416346d5385aa897d56465c39132
Sahil <70361613+sahil23032001@users.noreply.github.com>
2025-06-28 13:43:55 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"time"
+    "fmt"
+    "net/http"
+    "time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Current Date & Time: %s\n", time.Now().Format("Mon Jan 2 15:04:05 MST 2006"))
+    currentTime := time.Now().Format("Mon Jan 2 15:04:05 MST 2006")
+    fmt.Fprintf(w, "<h1>Date & Time</h1><p>%s</p>", currentTime)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", handler)
+    fmt.Println("Running on :8080")
+    http.ListenAndServe(":8080", nil)
 }
